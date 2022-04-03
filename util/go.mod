051099866d6251497942efebd2994db6bf2b17c3
Istvan Kiesz <istvan.kiesz@shapr3d.com>
2022-04-03 12:47:58 +0200

    adds maxint with lib

--- util/go.mod
+++ util/go.mod
@@ -1,3 +1,5 @@
 module github.com/kieszistvan/goplay/util
 
 go 1.17
+
+require github.com/thoas/go-funk v0.9.2 // indirect
--- util/strings/strings.go
+++ util/strings/strings.go
@@ -1,7 +1,15 @@
 package strings
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/thoas/go-funk"
+)
 
 func Sup(name string) string {
 	return fmt.Sprintf("Sup' %s", name)
+}
+
+func MaxInt(i ...int) int {
+	return funk.MaxInt(i)
 }
